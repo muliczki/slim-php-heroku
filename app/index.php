be76ba9e4a116200c804ebf824b7e7a78b0ca3f9
Micaela Uliczki <micauliczki@gmail.com>
2021-04-08 19:23:53 -0300

    .

--- app/index.php
+++ app/index.php
@@ -1,20 +1,31 @@
 <?php
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
 
-require_once '../vendor/autoload.php';
+include "Ej19_Vuelo.php";
 
+$pasajeros = array(new Pasajero("LOPEZ","CARLOS","21456217",TRUE));
 
+$pasajeros[] = new Pasajero("MARTINEZ","LUCAS","34561236",FALSE);
 
-$app = new \Slim\App([]);
+$pasajeros[] = new Pasajero("GOMEZ","MATIAS","41023674", FALSE);
+$pasajeros[] = new Pasajero("TRAIZA","MARCELA","25615890", TRUE);
 
+$unVuelo = new Vuelo("LATAM AIRLINES", 60000);
 
+$otroVuelo = new Vuelo("AMERICAN AIRLINES", 70000);
 
-$app->get('[/]', function (Request $request, Response $response) {    
-    $response->getBody()->write("GET => Bienvenido Micaela!!! ,a SlimFramework");
-    return $response;
+foreach ($pasajeros as $pasajero) {
+    echo $unVuelo->AgregarPasajero( $pasajero);
+}
 
-});
+echo $otroVuelo->AgregarPasajero( $pasajeros[3]);
+echo $unVuelo->GetVuelo();
 
+echo "Suma vuelo 1 y 2: $ ";
+echo number_format(Vuelo::Add($unVuelo, $otroVuelo),2); 
+echo "<br><br><br>";
 
-$app->run();
+Vuelo::Remove($unVuelo,$pasajeros[1]);
+Vuelo::Remove($unVuelo,$pasajeros[1]);
+echo $unVuelo->GetVuelo();
+
+?>
